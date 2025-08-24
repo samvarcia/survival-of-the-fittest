c4f83d9aff4f353f11eb5fe5e32c5369a8f28f62
Samuel Garcia <samvarcia@gmail.com>
2025-08-24 09:26:58 -0500

    blond

--- src/data/outfits.js
+++ src/data/outfits.js
@@ -31,8 +31,8 @@ export const outfits = [
   },
   {
     id: 7,
-    participantInstagram: "?",
-    image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5Glmq3iOTDLnOXd4BwTslftVWcp9DaC5SEuIb36"
+    participantInstagram: "sup3rprettyblonde",
+    image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlcSeojdQIFnWalKbj5yqv3GsEVpm91BixD6dk"
   },
   {
     id: 8,
@@ -51,71 +51,81 @@ export const outfits = [
   },
   {
     id: 11,
+    participantInstagram: "parkscanada_",
+    image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlkgsTlZ3vlYdoPNkK6gMi1eXqHJswzSQxj4Bn"
+  },
+  {
+    id: 12,
     participantInstagram: "ekighai",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlxAvD0ijPCcUhmiMk4V3zGWaA0qupfroR1ILJ"
   },
   {
-    id: 12,
+    id: 13,
     participantInstagram: "?",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlXBA34QWkSAK7Ugo6JLqjzpQYfZcDilmx9VIn"
   },
   {
-    id: 13,
+    id: 14,
     participantInstagram: "z3ke_sama",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlmRI4wNDLnOXd4BwTslftVWcp9DaC5SEuIb36"
   },
   {
-    id: 14,
+    id: 15,
     participantInstagram: "lottamercii",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5Gl2E7NQiMcpDihBKe1UMFJs0budo3RlqgSfHI5"
   },
   {
-    id: 15,
+    id: 16,
     participantInstagram: "okaymark0",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5Glz9Ip6SrhqtUPfDR13b9dwc2T0r6l4euyZaCY"
   },
   {
-    id: 16,
+    id: 17,
     participantInstagram: "?",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlNe1Q6al86sFjL7pBq1fMhwOxDtKYuZWlASIG"
   },
   {
-    id: 17,
-    participantInstagram: "sup3rprettyblonde",
-    image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlcSeojdQIFnWalKbj5yqv3GsEVpm91BixD6dk"
+    id: 18,
+    participantInstagram: "?",
+    image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5Glmq3iOTDLnOXd4BwTslftVWcp9DaC5SEuIb36"
   },
   {
-    id: 18,
+    id: 19,
     participantInstagram: "rasqas",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlXt6OFG2WkSAK7Ugo6JLqjzpQYfZcDilmx9VI"
   },
   {
-    id: 19,
+    id: 20,
+    participantInstagram: "beeudy",
+    image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlVOk6KCrLYobdpr6vmtZl3xa4w7JFzOqPN0iy"
+  },
+  {
+    id: 21,
     participantInstagram: "keysstatus",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlxGxIl3jPCcUhmiMk4V3zGWaA0qupfroR1ILJ"
   },
   {
-    id: 20,
+    id: 22,
     participantInstagram: "valectron",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GllAVeo4kGR0eAmZtr5cnyHCXYQ9hua2bKVz6U"
   },
   {
-    id: 21,
+    id: 23,
     participantInstagram: "timilehinnn_",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlOVFknULZKfS9sadLroeTAbYCBIQyMj32150G"
   },
   {
-    id: 22,
+    id: 24,
     participantInstagram: "_chantellea",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlDrBuNMlU48PSwRtzvbGndWMEThYI5FCofp31"
   },
   {
-    id: 23,
+    id: 25,
     participantInstagram: "hirakish_",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5Glrzf4y0ltiQwnKO2l03A8mpTGL1efPC6jMzs5"
   },
   {
-    id: 24,
+    id: 26,
     participantInstagram: "k_sznn",
     image: "https://v87ndduxgx.ufs.sh/f/ICfxMhSFP5GlY8mcBypThHMi36wVBSdPICxAvgotbQFfjsDn"
   },
